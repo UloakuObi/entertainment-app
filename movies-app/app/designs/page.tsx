2e32556eb452b9b97e8b4de48a9211f04a37d58d
UloakuObi <140077049+UloakuObi@users.noreply.github.com>
2026-03-12 13:09:25 +0000

    Initial setup

---
+++ movies-app/app/designs/page.tsx
@@ -0,0 +1,5 @@
+export default function Designs() {
+    return (
+        <h1>Custom Components Here</h1>
+    )
+}
---
+++ movies-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="m-4">
+      <h1 className="text-2xl font-medium">Home page</h1>
+      <p className="font-light">Testing paragraph</p>
+    </div>
+  );
+}
