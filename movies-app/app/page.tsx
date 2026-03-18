685ec8d000c9353689c4fb162739e8ba6a6e482c
uloakuobi <uloakuobi@gmail.com>
2026-03-18 14:51:59 +0000

    Initial setup

--- movies-app/app/page.tsx
+++ movies-app/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-4">
-      <h1 className="text-2xl font-medium">Home page</h1>
+      <h1 className="text-4xl font-light mb-6">Movies page</h1>
       <p className="font-light">Testing paragraph</p>
     </div>
   );
